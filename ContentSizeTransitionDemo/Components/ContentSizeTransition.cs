719ca3eef033f8437fce76b82377920eb3bd8a04
SlimeNull <slimenull@outlook.com>
2026-02-23 20:21:13 +0800

    添加项目文件。

---
+++ ContentSizeTransitionDemo/Components/ContentSizeTransition.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace ContentSizeTransitionDemo.Components
+{
+    public class ContentSizeTransition : ContentControl
+    {
+        private Size _targetMeasureSize;
+        private static readonly IEasingFunction _easing = new SineEase(){EasingMode = EasingMode.EaseOut};
+
+        public Size MeasureSizeAnimated
+        {
+            get { return (Size)GetValue(SizeOverrideProperty); }
+            set { SetValue(SizeOverrideProperty, value); }
+        }
+
+        public static readonly DependencyProperty SizeOverrideProperty =
+            DependencyProperty.Register(nameof(MeasureSizeAnimated), typeof(Size), typeof(ContentSizeTransition),
+                new FrameworkPropertyMetadata(default(Size), FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            var measureSizeAnimated = MeasureSizeAnimated;
+            var currentMeasureSize = base.MeasureOverride(constraint);
+
+            if (currentMeasureSize != _targetMeasureSize)
+            {
+                BeginAnimation(SizeOverrideProperty, null);
+                BeginAnimation(SizeOverrideProperty, new SizeAnimation
+                {
+                    From = DesiredSize,
+                    To = currentMeasureSize,
+                    Duration = TimeSpan.FromSeconds(0.3),
+                    FillBehavior = FillBehavior.HoldEnd,
+                    EasingFunction = _easing
+                });
+
+                _targetMeasureSize = currentMeasureSize;
+                return DesiredSize;
+            }
+
+            return measureSizeAnimated;
+        }
+
+        protected override Size ArrangeOverride(Size arrangeBounds)
+        {
+            if (VisualChildrenCount > 0 &&
+                GetVisualChild(0) is UIElement elementChild)
+            {
+                var childRect = new Rect(0, 0, arrangeBounds.Width, arrangeBounds.Height);
+                elementChild.Arrange(childRect);
+            }
+
+            return arrangeBounds;
+        }
+    }
+}
---
+++ ContentSizeTransitionDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContentSizeTransitionDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int _clickIndex = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ToggleContent_Click(object sender, RoutedEventArgs e)
+        {
+            _clickIndex++;
+            var height = _clickIndex % 2 == 0 ? 50 : 150;
+
+            container.Content = new TextBlock()
+            {
+                MinHeight = height,
+                Background = Brushes.AliceBlue,
+                Text = $"Height: {height}",
+            };
+        }
+    }
+}
